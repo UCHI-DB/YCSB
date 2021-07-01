bf12374de5b922fecc30f0d7616e852f0fed096e
harper <harperjiang@msn.com>
2021-07-01 11:04:26 -0500

    Writing JNI code

--- leveldb/src/main/jni/leveldb_jni.cc
+++ leveldb/src/main/jni/leveldb_jni.cc
@@ -2,7 +2,10 @@
 
 
 void JNICALL Java_site_ycsb_db_leveldb_LevelDB_init(JNIEnv *env, jobject caller, jbyteArray folder) {
-    env->GetByteArrayElements()
+    jint length = env->GetArrayLength(folder);
+    jbyte buffer[length];
+    env->GetByteArrayRegion(folder, 0, length, buffer);
+
 }
 
 void JNICALL Java_site_ycsb_db_leveldb_LevelDB_close(JNIEnv *env, jobject caller) {
